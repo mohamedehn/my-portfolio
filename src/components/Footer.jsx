e730367553e690ef9f2694a369bf18f02b85a8f4
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2023-03-16 20:51:29 +0100

    adding links in footer and small modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import '../styles/Footer.css'
 import TwitterIcon from "@mui/icons-material/Twitter"
 import LinkedInIcon from "@mui/icons-material/LinkedIn"
+import GithubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from "@mui/icons-material/Email";
 
 
 function Footer() {
@@ -11,6 +13,8 @@ function Footer() {
             <div className='socialMedia'>
                 <a href='https://twitter.com/ehn_dev'><TwitterIcon/></a>
                 <a href='https://fr.linkedin.com/in/mohamed-ebarhmatin-376731179'><LinkedInIcon/></a>
+                <a href='https://github.com/mohamedehn'><GithubIcon/></a>
+                <a href='mailto:mohamed.ebarhmatin@gmail.com'><EmailIcon/></a>
             </div>
             <p>&copy; 2022 Mohamed.Ebarhmatin</p>
         </div>
